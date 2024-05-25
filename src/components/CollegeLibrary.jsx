bb8f63a3772a59b30d6e9cc1662ee1ff57a59252
shibo911 <shivangagraharijee2023@gmail.com>
2024-05-25 12:56:41 +0530

    bug

--- src/components/CollegeLibrary.jsx
+++ src/components/CollegeLibrary.jsx
@@ -5,22 +5,22 @@ const CollegeLibrary = () => {
     <div class="absolute p-3 md:p-3 ml-64">
       <div class="columns-1 gap-2 lg:gap-2 sm:columns-2 lg:columns-3 xl:columns-4 [&>img:not(:first-child)]:mt-2 lg:[&>img:not(:first-child)]:mt-2">
 
-        <img src='../public/images/img2.png' alt="" />
-        <img src='../public/images/img3.png' alt="" />
-        <img src='../public/images/img1.png' alt="" />
-        <img src='../public/images/img4.png' alt="" />
-        <img src='../public/images/img5.png' alt="" />
-        <img src='../public/images/img6.png' alt="" />
-        <img src='../public/images/img7.png' alt="" />
-        <img src='../public/images/img8.png' alt="" />
-        <img src='../public/images/img13.png' alt="" />
-        <img src='../public/images/img9.png' alt="" />
-        <img src='../public/images/img10.png' alt="" />
-        <img src='../public/images/img11.png' alt="" />
-        <img src='../public/images/img12.png' alt="" />
-        <img src='../public/images/img14.png' alt="" />
-        <img src='../public/images/img15.png' alt="" />
-        <img src='../public/images/img16.png' alt="" />
+        <img src='/images/img2.png' alt="" />
+        <img src='/images/img3.png' alt="" />
+        <img src='/images/img1.png' alt="" />
+        <img src='/images/img4.png' alt="" />
+        <img src='/images/img5.png' alt="" />
+        <img src='/images/img6.png' alt="" />
+        <img src='/images/img7.png' alt="" />
+        <img src='/images/img8.png' alt="" />
+        <img src='/images/img13.png' alt="" />
+        <img src='/images/img9.png' alt="" />
+        <img src='/images/img10.png' alt="" />
+        <img src='/images/img11.png' alt="" />
+        <img src='/images/img12.png' alt="" />
+        <img src='/images/img14.png' alt="" />
+        <img src='/images/img15.png' alt="" />
+        <img src='/images/img16.png' alt="" />
         
 
       </div>
